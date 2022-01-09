8f4f7563623b3b84ff438c774a6258dcbb84fc4a
MichZiaj <mi.ziajkowski@gmail.com>
2022-01-09 16:25:18 +0100

    Add home Three Columns

--- oddam-w-dobre-rece/src/components/pages/Home_components/Home.js
+++ oddam-w-dobre-rece/src/components/pages/Home_components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HomeHead from './HomeHead';
+import HomeThreeColumns from "./HomeThreeColumns";
 
 
 const Home = () => {
     return (
         <>
            <HomeHead />
+            <HomeThreeColumns />
 
         </>
     );
